d60decd1425c39eb982c23a444f985b3109c5d59
Deden33 <93709979+Deden33@users.noreply.github.com>
2021-12-30 10:13:48 +0000

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,1 +1,47 @@
-II*AWo{"sticker-pack-id":"com.snowcorp.stickerly.android.stickercontentprovider b5e7275f-f1de-4137-961f-57becfad34f2","sticker-pack-name":"Made With bot-wea","sticker-pack-publisher":"","android-app-store-link":"https://play.google.com/store/apps/details?id=com.stickify.stickermaker","ios-app-store-link":"https://itunes.apple.com/app/sticker-maker-studio/id1443326857"}
+const { sticker1, sticker5 } = require('../lib/sticker')
+
+let handler = async (m, { conn }) => {
+    let stiker = false
+    try {
+        let q = m.quoted ? m.quoted : m
+        let mime = (q.msg || q).mimetype || ''
+        if (/webp/.test(mime)) {
+            let img = await q.download()
+            if (!img) throw `reply sticker with command s`
+            stiker = await sticker5(img, false, packname, author)
+        } else if (/image/.test(mime)) {
+            let img = await q.download()
+            if (!img) throw `reply image with command s`
+            stiker = await sticker5(img, false, packname, author)
+        } else if (/video/.test(mime)) {
+            if ((q.msg || q).seconds > 11) return m.reply('max is 10 seconds!')
+            let img = await q.download()
+            if (!img) throw `reply video with command s`
+            stiker = await sticker5(img, false, packname, author)
+        } else if (m.quoted.text) {
+            if (isUrl(m.quoted.text)) stiker = await sticker(false, m.quoted.text, packname, author)
+            else throw 'URL is not valid! end with jpg/gif/png'
+        }
+    } catch (e) {
+        throw e
+    }
+    finally {
+        if (stiker) {
+          m.reply(stiker_wait)
+            await conn.sendFile(m.chat, stiker, 'stiker.webp', '', m)
+        }
+        else {
+
+            throw 0
+        }
+    }
+}
+handler.help = ['sticker']
+handler.tags = ['sticker']
+handler.command = /^(stiker|s|sticker)$/i
+
+module.exports = handler
+
+const isUrl = (text) => {
+    return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))
+}
