eb0b4e4ff9017e6c54b0f95c80fa727053ee57c0
Deden33 <93709979+Deden33@users.noreply.github.com>
2021-12-30 10:06:05 +0000

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,17 +1,19 @@
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-module.exports = {
-  before(m, { isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe) return true
-    let chat = global.db.data.chats[m.chat]
-    let isGroupLink = linkRegex.exec(m.text)
+let handler = m => m
 
-    if (chat.antiLink && isGroupLink) {
-      m.reply('Hapus!!\n\nLink Grup terdeteksi')
-      if (global.opts['restrict']) {
-        if (isAdmin || !isBotAdmin) return true
-        // this.groupRemove(m.chat, [m.sender])
-      }
+let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { isAdmin, isBotAdmin }) {
+  if (m.isBaileys && m.fromMe) return true
+  let chat = global.db.data.chats[m.chat];
+  let ValidLink = (m.text.includes('https://') || m.text.includes('http://'))
+  if (chat.antiLink && ValidLink && !isAdmin && !m.isBaileys && m.isGroup) {
+    let thisGroup = isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}` : 0
+    if (m.text.includes(thisGroup) && thisGroup != 0) throw false // jika link grup itu sendiri gak dikick
+    await conn.reply(m.chat, `*Link Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *.on restrict* supaya bisa kick'}`, '', '', '', m)
+    if (global.opts['restrict']) {
+      if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
     }
-    return true
   }
+  return true
 }
+
+module.exports = handler
